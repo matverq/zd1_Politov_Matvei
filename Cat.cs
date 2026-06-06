a02a966d9d411a21833279fd897d8ba3722e2b2b
mightykk7 <ratovskijmaks@gmail.com>
2026-06-06 11:47:53 +0500

    Задание 1_1

---
+++ Cat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1_1_Politov_Matvei
+{
+    class Cat
+    {
+        private string name;
+        // Свойство с проверкой на верное значение имени
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                bool OnlyLetters = true;
+                foreach(var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+                if(OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+        }
+        private double mass;
+        // Свойство с проверкой на верные значения массы
+        public double Mass
+        {
+            get
+            {
+                return mass;
+            }
+            set
+            {
+                if(value > 0 && value < 21)
+                {
+                    mass = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильный вес!!!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+        }
+        // Конструктор класса
+        public Cat(string CatName, double CatMass)
+        {
+            Name = CatName;
+            Mass = CatMass;
+        }
+        // Вывод информаци
+        public void Meow()
+        {
+            Console.WriteLine($"Кот: {name} вес: {Mass} - МЯЯЯЯУ!!!!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1_1_Politov_Matvei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите количество котов");
+                int count = Convert.ToInt32(Console.ReadLine());
+
+                if (count <=0)
+                {
+                    Console.WriteLine("Введено неверное значение!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
+                Cat cats = new Cat("", 1);
+                
+                for (int i = 0; i < count; i++)
+                {
+                    cats = new Cat("", 1);
+                    Console.WriteLine($"Кот: {i + 1}");
+                    Console.WriteLine("Введите имя кота:");
+                    cats.Name = Console.ReadLine();
+                    Console.WriteLine("Введите вес кота");
+                    cats.Mass = Convert.ToDouble(Console.ReadLine());
+                    cats.Meow();
+                }
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Введено неверное значение!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
